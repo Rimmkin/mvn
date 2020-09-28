d1766bd12bd8fe06f231fea871a76f6dbd24529c
Rimmkin <70262551+Rimmkin@users.noreply.github.com>
2020-09-28 19:22:57 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -33,4 +33,24 @@ class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+ @Test
+        void shouldCalculateForNotRegisteredAndUnderLimit() {
+            BonusService service = new BonusService();
+            long amount = 1000_60;
+            boolean registered = false;
+            long expected = 10;
+            long actual = service.calculate(amount, registered);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForNotRegisteredAndOverLimit() {
+            BonusService service = new BonusService();
+            long amount = 1_000_000_60;
+            boolean registered = false;
+            long expected = 500;
+            long actual = service.calculate(amount, registered);
+            assertEquals(expected, actual);
+
+    }
 }
