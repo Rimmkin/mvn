45d68f4dddd5efd4ea09700bc005837abfada5b3
Rimmkin <70262551+Rimmkin@users.noreply.github.com>
2020-09-29 12:48:09 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -38,7 +38,7 @@ class BonusServiceTest {
             BonusService service = new BonusService();
             long amount = 1000_60;
             boolean registered = false;
-            long expected = 10;
+            long expected = 30;
             long actual = service.calculate(amount, registered);
             assertEquals(expected, actual);
         }
